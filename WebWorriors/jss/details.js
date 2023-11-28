4f111d30fbcf62db682521c52bd23e96bc62b59c
bbongnamull <144579090+bbongnamull@users.noreply.github.com>
2023-11-28 19:58:07 +0900

    상세페이지 관련 js입니다

---
+++ WebWorriors/jss/details.js
@@ -0,0 +1,47 @@
+function updateTotalPrice() {
+        var quantity = document.getElementById('quantity').value;
+        var unitPrice = 34400; // Assuming unit price is 34,400원, you can adjust this value
+
+        var totalPrice = quantity * unitPrice;
+        document.getElementById('total-price').innerText = totalPrice.toLocaleString('en-US') + '원';
+    }
+    function showImages() {
+            var container = document.getElementById("imageContainer");
+
+            // Toggle the 'partial' class for the container
+            container.classList.toggle("partial");
+
+            // Update the button text based on the container class
+            var buttonText = container.classList.contains("partial") ? "전체 이미지 보기" : "초기 이미지 보기";
+            document.getElementById("image-container-button").innerText = buttonText;
+        }
+        // 팝업 열기
+    function openPopup() {
+        var modal = document.getElementById('myModal');
+        modal.style.display = 'block';
+        }
+
+// 팝업 닫기
+    function closePopup() {
+        var modal = document.getElementById('myModal');
+        modal.style.display = 'none';
+    }
+
+// 모달 영역 외의 부분 클릭 시 팝업 닫기
+    window.onclick = function(event) {
+        var modal = document.getElementById('myModal');
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+    // Function to open 선물함 modal
+function openGiftPopup() {
+    var giftModal = document.getElementById("giftModal");
+    giftModal.style.display = "block";
+}
+
+// Function to close 선물함 modal
+function closeGiftPopup() {
+    var giftModal = document.getElementById("giftModal");
+    giftModal.style.display = "none";
+}
