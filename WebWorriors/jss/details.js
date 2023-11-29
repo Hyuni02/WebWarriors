9e79e0f5bb538c6a72b1d8dcc416e57130b3783e
bbong <brain9967@gmail.com>
2023-11-29 18:54:09 +0900

    전반적인 작업 완료 확인해보시고 수정 사항 말씀해주세요

--- WebWorriors/jss/details.js
+++ WebWorriors/jss/details.js
@@ -16,16 +16,18 @@ function updateTotalPrice() {
             document.getElementById("image-container-button").innerText = buttonText;
         }
         // 팝업 열기
-    function openPopup() {
-        var modal = document.getElementById('myModal');
-        modal.style.display = 'block';
-        }
+        function openPopup() {
+            
+            var modal = document.getElementById("myModal");
+            modal.style.display = "block";
+          }
+          
 
 // 팝업 닫기
-    function closePopup() {
-        var modal = document.getElementById('myModal');
-        modal.style.display = 'none';
-    }
+function closePopup() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
 
 // 모달 영역 외의 부분 클릭 시 팝업 닫기
     window.onclick = function(event) {
@@ -35,13 +37,14 @@ function updateTotalPrice() {
         }
     }
     // Function to open 선물함 modal
-function openGiftPopup() {
-    var giftModal = document.getElementById("giftModal");
-    giftModal.style.display = "block";
+    function openGiftPopup() {
+    // Display the gift popup
+    var giftModal = document.getElementById('giftModal');
+    giftModal.style.display = 'block';
 }
-
 // Function to close 선물함 modal
 function closeGiftPopup() {
-    var giftModal = document.getElementById("giftModal");
-    giftModal.style.display = "none";
+    // Close the gift popup
+    var giftModal = document.getElementById('giftModal');
+    giftModal.style.display = 'none';
 }
